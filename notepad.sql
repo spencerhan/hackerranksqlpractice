7423fe6752862658c9019f2f4f2f35308f8b6525
S.H <spencer.default.dev@gmail.com>
2022-02-26 00:32:00 +1300

    26/02/2022

--- notepad.sql
+++ notepad.sql
@@ -195,9 +195,7 @@ ORDER BY COUNT(h.hacker_id) desc, h.hacker_id asc;
 
 SELECT id, age, coins_needed, power
 FROM (SELECT w.id AS id, wp.age AS age, w.coins_needed AS coins_needed, MIN(w.coins_needed) OVER (PARTITION BY w.power, wp.age) AS min_coins, w.power as power
-    FROM Wands AS w 
-    INNER JOIN Wands_Property AS wp 
-    ON w.code=wp.code
+    FROM Wands AS w INNER JOIN Wands_Property AS wp  ON w.code=wp.code
     WHERE wp.is_evil=0) AS t
 WHERE min_coins=coins_needed
 ORDER BY power DESC, age DESC;
@@ -857,128 +855,40 @@ WHERE book_id NOT IN (
                         SELECT book_id
                         FROM Orders
                         GROUP BY book_id
-                        HAVING SUM(CASE WHEN dispatch_date >= DATEADD(YEAR, -1, '2019-06-23' ) THEN quantity else 0 end) >= 10
+                        HAVING sum(CASE WHEN dispatch_date >= DATEADD(YEAR, -1, '2019-06-23' ) THEN quantity else 0 end) >= 10
                     UNION (
                              SELECT book_id
                              FROM Books
-                             WHERE available_from > DATEADD(MONTH, -1, '2019-06-23' ))) 
+                             WHERE available_from > DATEADD(MONTH, -1, '2019-06-23' )))  
 
 
--- 1107. New Users Daily Count, https://leetcode.com/problems/new-users-daily-count/
+SELECT user_id, activity,  ROW_NUMBER() OVER (PARTITION BY user_id, activity_date) as rnk, activity_date
+FROM Traffic
+WHERE activity_date >= DATEADD(day, -90, '2019-06-30') AND activity = 'login'
 
-/* main logic, use ROW_NUMBER to get the first login in every date. */
-WITH t1 AS (
-            SELECT *, ROW_NUMBER() OVER (PARTITION BY user_id ORDER BY activity_date) as rnumber
-            FROM Traffic 
-            WHERE activity = 'login'
-            
-),
-t2 AS (SELECT *
-       FROM t1
-       WHERE activity_date >= DATEADD(day, -90, '2019-06-30')
-)
-SELECT activity_date as login_date, COUNT(rnumber) as user_count
-FROM t2
-WHERE rnumber = 1
-GROUP BY activity_date
-
-/* solution number 2: */
-BEGIN
-SELECT A.activity_date AS login_date, COUNT(DISTINCT A.user_id) AS user_count
-FROM (
-      SELECT user_id,activity_date
-      FROM Traffic 
-      WHERE activity_date >= DATEADD(day, -90, '2019-06-30')) A
-JOIN (
-      SELECT user_id,min(activity_date) AS minDate 
-      FROM Traffic
-      WHERE activity = 'login'
-      GROUP BY user_id ) B
-ON A.user_id = B.user_id and A.activity_date = B.minDate
-GROUP BY A.activity_date
-END
-
-/* 1112. Highest Grade For Each Student, https://leetcode.com/problems/highest-grade-for-each-student/
- main logic: window function */
-BEGIN 
-WITH t1 AS (SELECT max(grade) as maxgrade, student_id
-FROM Enrollments
-GROUP BY student_id)
-,t2 AS (
-    SELECT t1.maxgrade, t1.student_id, row_number() OVER (partition by t1.student_id order by course_id) as rownum, Enrollments.course_id as course_id
-    FROM t1
-    JOIN Enrollments
-    ON Enrollments.student_id = t1.student_id AND t1.maxgrade = grade
-)
-SELECT t2.student_id, t2.course_id, t2.maxgrade as grade
-FROM t2
-WHERE t2.rownum = 1
-ORDER BY student_id asc
-END
-
--- 1126. Active Businesses, https://leetcode.com/problems/active-businesses/
-/* main logic: CTE */
-BEGIN
-WITH t1 AS (
-           SELECT AVG(occurences) AS benchmark, event_type
-           FROM Events e1
-           GROUP BY event_type
-),
-t2 AS (
-    SELECT e2.business_id AS business_id
-    FROM Events e2
-    JOIN t1
-    ON e2.event_type = t1.event_type AND e2.occurences > t1.benchmark
-
-)
-SELECT business_id 
-FROM t2
-GROUP BY business_id 
-HAVING count(business_id) > 1;
-END
-
-
--- 1132. Reported Posts II, https://leetcode.com/problems/reported-posts-ii/
-
-/* main logic:  
-1. use "* 100.00" in calculation - this gives two decimal places in the result
-2. use distcint - multiple spam can be reported and removed during the same day.
-3. use left-join - use this when table on one side is larger than the other side. 
+-- 1164. Product Price at a Given Date, https://leetcode.com/problems/product-price-at-a-given-date/
+/* main logic: 
+step 1: get a list of product that has changed price before the given date 
+step 2: use row number to order the step 1 subset based on the date column.
+step 3: select row number = 1 to get the most recent price
+step 4: get a list of products regardless whether they have changed price or not
+step 5: left join or right join with a case statement to default the price to 10 dollers for products that have not changed price before the given date
 */
-
 WITH t1 AS (
-        SELECT COUNT(DISTINCT r.post_id) * 100.00/COUNT(DISTINCT a.post_id) AS avg_spam_per_post
-        FROM Actions a
-        LEFT JOIN Removals r
-        ON a.post_id = r.post_id
-        WHERE a.action = 'report' AND a.extra = 'spam'
-        GROUP BY a.action_date
+    SELECT product_id, new_price as price
+    FROM
+        (SELECT product_id, new_price, ROW_NUMBER() OVER (partition BY product_id ORDER BY change_date desc) as rownum
+        FROM Products
+        WHERE change_date <= '2019-08-16') t
+    WHERE rownum = 1
+), t2 AS (
+        SELECT DISTINCT product_id
+        FROM Products
 )
-
-SELECT ROUND(AVG(avg_spam_per_post),2) AS average_daily_percent 
+SELECT t2.product_id, (CASE
+                       WHEN t1.price IS NULL THEN 10
+                       ELSE t1.price
+                      END) as price
 FROM t1
-
-
--- 1149. Article Views II, https://leetcode.com/problems/article-views-ii/
-/* main logic: simple grouping and having  */
-SELECT DISTINCT viewer_id as id
-FROM Views
-GROUP BY viewer_id, view_date
-HAVING COUNT(DISTINCT article_id) > 1
-ORDER BY id asc;
-
-
-
--- 1158. Market Analysis I, https://leetcode.com/problems/market-analysis-i/
-/* main logic: simple subquery with left join
- Remember, whichever side is larger, use the left/right join prefer the larger side.
-*/
-
-SELECT user_id AS buyer_id, join_date, COUNT(t.buyer_id) AS orders_in_2019
-FROM Users
-LEFT JOIN (SELECT buyer_id
-           FROM Orders
-           WHERE order_date >= '2019-01-01') t
-ON Users.user_id = t.buyer_id
-GROUP BY user_id, join_date
-ORDER BY user_id
+RIGHT JOIN t2
+ON t1.product_id = t2.product_id
