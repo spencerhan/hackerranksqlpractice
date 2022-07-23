963f13ae928b07ddfc5c14cdcfe5ca7988dd636b
ouhshuo 麥普 <spencer.default.dev@gmail.com>
2022-07-23 23:40:55 +1200

    23/07/2022

--- difficult_notepad.sql
+++ difficult_notepad.sql
@@ -95,77 +95,86 @@ OPTION (maxrecursion 0)
 
 --2153. The Number of Passengers in Each Bus II, https://leetcode.com/problems/the-number-of-passengers-in-each-bus-ii/
 /* ORDER BY used for running total */
-
-WITH t1 AS (
-    SELECT
-        bus_id,
-        arrival_time,
-        LAG(arrival_time, 1, -1) OVER (
-            ORDER BY
-                arrival_time
-        ) AS pre_bus_time,
-        ROW_NUMBER() OVER (
-            ORDER BY
-                arrival_time
-        ) AS rn,
-        capacity
-    FROM
-        Buses
-),
-t2 AS (
-    SELECT
-        t1.rn,
-        t1.bus_id,
-        t1.capacity,
-        COUNT(p.passenger_id) AS current_passenger_cnt
-    FROM
-        t1
-        LEFT JOIN Passengers p ON p.arrival_time <= t1.arrival_time
-        AND p.arrival_time > t1.pre_bus_time
-    GROUP BY
-        t1.rn,
-        t1.bus_id,
-        t1.capacity
-),
-recursive_cte AS (
-    SELECT
-        rn,
-        bus_id,
-        capacity,
-        current_passenger_cnt,
-        IIF(
-            current_passenger_cnt - capacity <= 0,
-            0,
-            current_passenger_cnt - capacity
-        ) AS remaining_passenger_cnt
-    FROM
-        t2
-    WHERE
-        rn = 1
-    UNION
-    ALL
-    SELECT
-        t2.rn,
-        t2.bus_id,
-        t2.capacity,
-        t2.current_passenger_cnt + rc.remaining_passenger_cnt AS current_passenger_cnt,
-        IIF(
-            t2.current_passenger_cnt + rc.remaining_passenger_cnt - t2.capacity <= 0,
-            0,
-            t2.current_passenger_cnt + rc.remaining_passenger_cnt - t2.capacity
-        ) AS remaining_passenger_cnt
-    FROM
-        recursive_cte rc
-        JOIN t2 ON t2.rn = rc.rn + 1
+WITH t1 AS ( -- bus arriving window and arriving order
+    SELECT bus_id, arrival_time, capacity, ROW_NUMBER() OVER (ORDER BY arrival_time) AS rn, LAG(arrival_time, 1, -1) OVER (ORDER BY arrival_time) AS prev_arrival_time
+    FROM Buses 
+    
+), t2 AS ( -- attaching arriving window to passengers, counting passengers count
+    SELECT t1.bus_id, t1.rn, t1.capacity, COUNT(p.passenger_id) AS passenger_cnt
+        FROM t1
+        LEFT JOIN Passengers p -- using left join as we have to multiple passengers arrival at the same arrival window.
+        ON p.arrival_time > t1.prev_arrival_time AND p.arrival_time <= t1.arrival_time
+        GROUP BY t1.bus_id, t1.rn, t1.capacity
 )
-SELECT
-    bus_id,
-    IIF(
-        remaining_passenger_cnt > 0,
-        capacity,
-        current_passenger_cnt
-    ) AS passengers_cnt
-FROM
-    recursive_cte
-ORDER BY
-    bus_id
+
+
+-- 15 days of sql, https://www.hackerrank.com/challenges/15-days-of-learning-sql/problem?isFullScreen=false
+/* https://medium.com/geekculture/15-days-of-learning-sql-hackerrank-a40ab17ae462 */
+
+WITH t1 AS ( -- count of submission per day per hacker
+    SELECT submission_date, hacker_id, COUNT(1) AS cnt
+    FROM Submissions
+    GROUP BY submission_date, hacker_id
+), t2 AS ( -- getting the rank of submission for each hacker per day
+    SELECT submission_date, hacker_id, RANK() OVER (PARTITION BY submission_date ORDER BY cnt DESC, hacker_id) AS rnk
+    FROM t1
+), t3 AS ( -- keeps tracking the submission day counts in sequences
+    SELECT submission_date, hacker_id, DENSE_RANK() OVER (ORDER BY submission_date) AS dayrnk
+    FROM Submissions
+), t4 AS ( -- check for each hacker, whether they have made submission prior to the days earlier than each submission day
+           -- the first day, 2016-03-01, hackers who made the submission should be counted.
+    SELECT t3.submission_date, t3.hacker_id, 
+        CASE
+            WHEN t3.submission_date = '2016-03-01' THEN 1
+            ELSE 1 + (
+                        SELECT COUNT(DISTINCT s.submission_date) 
+                        FROM Submissions s 
+                        WHERE s.hacker_id = t3.hacker_id AND s.submission_date < t3.submission_date
+                    )
+            END AS prevsub_cnt, 
+        t3.dayrnk
+    FROM t3
+), t5 AS ( -- if a hacker is continuously making submissions each day, 
+            -- then his total submissions should match the day counts
+
+    SELECT submission_date, COUNT(DISTINCT hacker_id) AS hacker_cnt
+    FROM t4 
+    WHERE prevsub_cnt = dayrnk
+    GROUP BY submission_date
+)
+-- combine everything together.
+SELECT t5.submission_date, t5.hacker_cnt, t2.hacker_id, h.name
+FROM t5
+JOIN t2
+ON t5.submission_date = t2.submission_date
+JOIN Hackers h
+ON h.hacker_id = t2.hacker_id
+WHERE t2.rnk = 1
+
+
+-- Print Prime Number, https://www.hackerrank.com/challenges/print-prime-numbers/problem?isFullScreen=true
+
+DECLARE @i AS INT = 2;
+DECLARE @prime INT = 0;
+DECLARE @t AS TABLE (result INT)
+WHILE @i <= 1000
+BEGIN 
+    DECLARE @j INT = @i - 1;
+    SET @prime = 1
+    WHILE @j > 1
+    BEGIN
+        IF @i % @j = 0
+        BEGIN
+            SET @prime = 0;
+        END
+        SET @j = @j - 1;
+    END
+    IF @prime = 1
+    BEGIN 
+        INSERT INTO @t (result) VALUES (@i)
+    END 
+SET @i = @i + 1
+END
+DECLARE @result VARCHAR(MAX);
+SELECT @RESULT = STRING_AGG(result, '&') FROM @t;
+PRINT @result;
